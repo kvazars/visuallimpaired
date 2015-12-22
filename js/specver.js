b3c444086ccf3456356e28e2b8f19da485fb18dc
Viktor Konobeyev <mr.kvazar@gmail.com>
2015-12-22 20:35:40 +0500

    Update specver.js

--- js/specver.js
+++ js/specver.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 function loadform(){
-  $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\");location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
+  $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
 $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" );  
 }    
 
